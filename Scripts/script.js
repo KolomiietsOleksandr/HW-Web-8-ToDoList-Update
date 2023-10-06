70a6c0d3ef7f4994b721b1b1096a661c634b5a03
Oleksandr Kolomiiets <alexcrimson0818@gmail.com>
2023-10-06 17:28:39 +0300

    First task

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,23 +1,86 @@
+// Parent class TodoItem
+class TodoItem {
+    constructor(text, date) {
+        this.text = text;
+        this.date = date;
+    }
+
+    display() {
+        return `
+            <input type="checkbox">
+            <span>${this.text}</span>
+            <span class="date">${this.date}</span>
+            <button class="delete">Delete</button>
+        `;
+    }
+}
+
+// Child class TodoItemPremium that extends TodoItem
+class TodoItemPremium extends TodoItem {
+    constructor(text, date, iconUrl) {
+        super(text, date);
+        this.iconUrl = iconUrl;
+    }
+
+    display() {
+        return `
+            <input type="checkbox">
+            <span>${this.text}</span>
+            <span class="date">${this.date}</span>
+            <img src="${this.iconUrl}" alt="Icon" class="icon">
+            <button class="delete">Delete</button>
+        `;
+    }
+}
+
 const taskInput = document.getElementById('task');
+const premiumTaskCheckbox = document.getElementById('premiumTask');
+const iconFileContainer = document.getElementById('iconFileContainer');
+const iconFileInput = document.getElementById('iconFile');
 const taskList = document.getElementById('task-list');
 const clearCompletedButton = document.getElementById('clear-completed');
 const clearAllButton = document.getElementById('clear-all');
+
+// Initially hide the icon upload field
+iconFileContainer.style.display = 'none';
+
+// Add an event listener to check the premium task checkbox
+premiumTaskCheckbox.addEventListener('change', function () {
+    if (premiumTaskCheckbox.checked) {
+        // Show the icon upload field when the checkbox is checked
+        iconFileContainer.style.display = 'block';
+    } else {
+        // Hide the icon upload field when the checkbox is unchecked
+        iconFileContainer.style.display = 'none';
+        // Clear the value of the icon upload input
+        iconFileInput.value = '';
+    }
+});
 
 function addTask() {
     const taskText = taskInput.value.trim();
     if (taskText === '') return;
 
-    const taskItem = document.createElement('li');
     const date = new Date().toLocaleString();
-    taskItem.innerHTML = `
-        <input type="checkbox">
-        <span>${taskText}</span>
-        <span class="date">${date}</span>
-        <button class="delete">Delete</button>
-    `;
+    let todoItem;
+
+    if (premiumTaskCheckbox.checked && iconFileInput.files.length > 0) {
+        // Premium task with a checked checkbox and uploaded icon
+        const iconFile = iconFileInput.files[0];
+        const iconUrl = URL.createObjectURL(iconFile);
+        todoItem = new TodoItemPremium(taskText, date, iconUrl);
+    } else {
+        // Regular task or premium task without a checked checkbox or uploaded icon
+        todoItem = new TodoItem(taskText, date);
+    }
+
+    const taskItem = document.createElement('li');
+    taskItem.innerHTML = todoItem.display();
 
     taskList.prepend(taskItem);
     taskInput.value = '';
+    iconFileInput.value = ''; // Clear the icon upload input
+    premiumTaskCheckbox.checked = false; // Uncheck the premium task checkbox
     updateEventListeners();
 }
 
